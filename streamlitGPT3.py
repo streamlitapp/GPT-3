007e5821dff5a97404a7bb8f16880d5ea812db59
streamlitapp <111313839+streamlitapp@users.noreply.github.com>
2022-12-31 23:11:19 -0500

    Update streamlitGPT3.py

--- streamlitGPT3.py
+++ streamlitGPT3.py
@@ -3,7 +3,7 @@ import openai
 
 st.subheader('GPT-3')
 
-openai.api_key = "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"
+openai.api_key = "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"
 
 model_engine = st.selectbox('Engine', 
                             ('text-davinci-003', 'text-curie-001', 'text-babbage-001', 'text-ada-001',
