f5df35249d1a76af19f5338791ea0e787900179c
streamlitapp <111313839+streamlitapp@users.noreply.github.com>
2022-12-31 23:49:10 -0500

    Update streamlitGPT3.py

--- streamlitGPT3.py
+++ streamlitGPT3.py
@@ -4,7 +4,7 @@ import openai
 st.subheader('GPT-3')
 
 api_key = st.text_input('API Key')
-engine = st.selectbox('Engine', ('text-davinci-003', 'text-curie-001', 'text-babbage-001', 'text-ada-001', 'code-davinci-002', 'code-cushman-001'))
+engine = st.selectbox('Engine', ('text-davinci-003', 'text-davinci-002', 'text-curie-001', 'text-babbage-001', 'text-ada-001', 'code-davinci-002', 'code-cushman-001'))
 max_tokens = st.number_input('Max Tokens', value = 4000, step = 1)
 prompt = st.text_input('Prompt')
 
